9629a104e8692634d26afee7422faace941e905f
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-24 09:38:38 -0600

    adicion de base de datos y cambio modfal

---
+++ db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE notas (
+    id SERIAL PRIMARY KEY,
+    titutlo VARCHAR(255),
+    categoria VARCHAR(255),
+    tema VARCHAR(255),
+    fuente VARCHAR(255),
+    contenido TEXT,
+    fecha DATE,
+    autor VARCHAR(255),
+    link VARCHAR(255)
+)
