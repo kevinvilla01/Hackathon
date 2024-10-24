c56c879e28c1ec1c579829e8a21df7585b380f28
kevinvilla01 <kevin01villa0101@gmail.com>
2024-10-24 12:35:08 -0600

    commits

--- backend/mandarNot.php
+++ backend/mandarNot.php
@@ -9,5 +9,16 @@ if (!$conexion) {
     die("No se pudo conectar a la base de datos: " . pg_last_error());
 }
 
-$consulta = "SELECT * FROM usuarios";
+$sql = "INSERT INTO notas () VALUES ('Liz', 100)";
+$resultado = pg_query($conexion, $sql);
+
+if (!$resultado) {
+    die("No se pudo ejecutar el comando: " . pg_last_error());
+}
+
+while ($fila = pg_fetch_assoc($resultado)) {
+    echo $fila['nombre'] . "\n";
+}
+
+pg_close($conexion);
 ?>
